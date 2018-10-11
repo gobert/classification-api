adef1516aa89c5187ea5c44ae9c3e246610b4ab0
Geoffroy GOBERT <gobertgeoffroy@gmail.com>
2018-10-11 19:48:06 +0200

    Complete Data Model: Prediction = Algo + Picture
    Add a data model in 3 classes:
    Algorithm *--* Picture
    
    So we create the model Prediction that apply on one algorithm and
    one picture.
    
    The model adds also relevant fields and associations between models.

--- classification_api.py
+++ classification_api.py
@@ -1,26 +1,110 @@
 from application import Application
-import sqlalchemy
 import datetime
+
+from sqlalchemy import Table, Column, Integer, Float, String, ForeignKey, DateTime, ARRAY
+from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
+
 
 app = Application.get_instance()
 
 
 class Picture(app.Base):
+    """
+        Represents a picture in the databse. On this pictures will algorithmes
+        tested on to recognize if the product is good or defect.
+    """
     __tablename__ = 'pictures'
 
-    id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True)
-    image_id = sqlalchemy.Column(sqlalchemy.String(255))
-    image_path = sqlalchemy.Column(sqlalchemy.String(1024))
+    id = Column(Integer, primary_key=True)
+    predictions = relationship('Prediction', back_populates='picture')
 
-    created_at = sqlalchemy.Column(sqlalchemy.DateTime())
-    updated_at = sqlalchemy.Column(sqlalchemy.DateTime())
+    image_id = Column(String(255), index=True)
+    image_path = Column(String(1024))
 
-    def __init__(self, image_id=None):
+    created_at = Column(DateTime())
+    updated_at = Column(DateTime())
+
+    def __init__(self, image_id=None, image_path=None):
         self.image_id = image_id
+        self.image_path = image_path
 
     @classmethod
-    def create(cls, image_id=None):
-        record = cls(image_id=image_id)
+    def create(cls, image_id=None, image_path=None):
+        record = cls(image_id=image_id, image_path=image_path)
+        record.created_at = datetime.datetime.now()
+        record.updated_at = datetime.datetime.now()
+
+        app.db_session.add(record)
+        app.db_session.commit()
+
+        return record
+
+
+class Algorithm(app.Base):
+    """
+        Represents an predictive model algorithm on the computer. Those
+        algorithm will predict if pictures represents a good or defect product.
+    """
+    __tablename__ = 'algorithms'
+
+    id = Column(Integer, primary_key=True)
+    predictions = relationship('Prediction', back_populates='algorithm')
+
+    name = Column(String(255), index=True)
+    version = Column(String(255), index=True)
+
+    created_at = Column(DateTime())
+    updated_at = Column(DateTime())
+
+    def __init__(self, name=None, version=None):
+        self.name = name
+        self.version = version
+
+    @classmethod
+    def create(cls, name=None, version=None):
+        record = cls(name=name, version=version)
+        record.created_at = datetime.datetime.now()
+        record.updated_at = datetime.datetime.now()
+
+        app.db_session.add(record)
+        app.db_session.commit()
+
+        return record
+
+
+class Prediction(app.Base):
+    """
+        Represent the outcome of an algorihm on a picture.
+    """
+    __tablename__ = 'predictions'
+
+    id = Column(Integer, primary_key=True)
+
+    proba = Column(Float(), index=True)
+    label = Column(String(255))
+    result = Column(String(255))
+    bbox = Column(ARRAY(Float))
+
+    created_at = Column(DateTime())
+    updated_at = Column(DateTime())
+
+    algorithm_id = Column(Integer, ForeignKey('algorithms.id'), index=True)
+    algorithm = relationship('Algorithm', back_populates='predictions')
+    picture_id = Column(Integer, ForeignKey('pictures.id'), index=True)
+    picture = relationship('Picture', back_populates='predictions')
+
+    def __init__(self, proba=None, label=None, result=None, bbox=None, algorithm_id=None, picture_id=None):
+        self.proba = proba
+        self.label = label
+        self.result = result
+        self.bbox = bbox
+        self.algorithm_id = algorithm_id
+        self.picture_id = picture_id
+
+    @classmethod
+    def create(cls, proba=None, label=None, result=None, bbox=None, algorithm_id=None, picture_id=None):
+        record = cls(proba=proba, label=label, result=result, bbox=bbox, algorithm_id=algorithm_id, picture_id=picture_id)
         record.created_at = datetime.datetime.now()
         record.updated_at = datetime.datetime.now()
 
--- tests.py
+++ tests.py
@@ -2,12 +2,96 @@ import os
 from application import Application
 app = Application.get_instance('test')
 app.init_db()
-from classification_api import Picture
+from classification_api import Picture, Algorithm, Prediction
+import pytest
+from freezegun import freeze_time
+import datetime
 
 
 class TestPicture():
-    def test_picture_init(self):
-        assert Picture(image_id='foo').image_id == 'foo'
+    @pytest.fixture
+    def image_id(self):
+        return '234567890'
 
-    def test_picture_create(self):
-        assert Picture.create(image_id='foo').image_id == 'foo'
+    @pytest.fixture
+    def image_path(self):
+        return '20181011/234567890.jpg'
+
+    def test_picture_init(self, image_id):
+        assert Picture(image_id=image_id).image_id == image_id
+
+    def test_picture_create_image_id(self, image_id):
+        assert Picture.create(image_id=image_id).image_id == image_id
+
+    def test_picture_create_image_path(self, image_path):
+        assert Picture.create(image_path=image_path).image_path == image_path
+
+    def test_picture_create_created_at(self):
+        now = freeze_time('2018-10-11 11:26:01')
+        now.start()
+        assert Picture.create().created_at ==datetime.datetime(2018, 10, 11, 11, 26, 1)
+        now.stop()
+
+
+class TestAlgorithm():
+    @pytest.fixture
+    def name(self):
+        return 'DeevioNet'
+
+    @pytest.fixture
+    def version(self):
+        return '1.0'
+
+    def test_algorithm_init(self, name):
+        assert Algorithm(name=name).name == name
+
+    def test_algorithm_create_name(self, name):
+        assert Algorithm.create(name=name).name == name
+
+    def test_algorithm_create_version(self, version):
+        assert Algorithm.create(version=version).version == version
+
+
+class TestPrediction():
+    # algorithm_id = Column(Integer, ForeignKey('algorithms.id'))
+    # picture_id = Column(Integer, ForeignKey('pictures.id'))
+
+    @pytest.fixture
+    def proba(self):
+        return 0.99999
+
+    @pytest.fixture
+    def label(self):
+        return 'nails'
+
+    @pytest.fixture
+    def bbox(self):
+        return [1008.8, 280.6, 1110.0, 380.72021484375]
+
+    @pytest.fixture
+    def result(self):
+        return 'good'
+
+    def test_prediction_init(self, label):
+        assert Prediction(label=label).label == label
+
+    def test_prediction_create_proba(self, proba):
+        assert Prediction.create(proba=proba).proba == proba
+
+    def test_prediction_create_bbox(self, bbox):
+        assert Prediction.create(bbox=bbox).bbox == bbox
+
+    def test_prediction_create_result(self, result):
+        assert Prediction.create(result=result).result == result
+
+    def test_prediction_create_algorithm(self):
+        algorithm = Algorithm.create()
+        prediction = Prediction.create(algorithm_id=algorithm.id)
+        assert prediction.algorithm == algorithm
+        assert algorithm.predictions[-1] == prediction
+
+    def test_prediction_create_picture(self):
+        picture = Picture.create()
+        prediction = Prediction.create(picture_id=picture.id)
+        assert prediction.picture == picture
+        assert picture.predictions[-1] == prediction
